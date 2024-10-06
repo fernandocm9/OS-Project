3241706a24eec1ecfb83e1b6865ac08dedd4f14d
fernandocm9 <88690826+fernandocm9@users.noreply.github.com>
2024-10-06 13:06:56 -0400

    Created the part that creates the regex compilation but there is either something wrong with the regexec or the input handling because it will not check properly. All of this is in the URLtoFile function.

--- main.c
+++ main.c
@@ -2,10 +2,31 @@
 # include <errno.h>
 # include <fcntl.h>
 # include <unistd.h>
-# include <time.h>
+# include <regex.h>
 
 void URLtoFile(){
+    regex_t rx; //var to create regex
+    int status; // to store value if regex compile was successfull or not
 
+    char inputURL[100];
+
+    status = regcomp(&rx, "^((https?://)?[[:alnum:]]+[.]+com|((https?://)?[[:alnum:]]+[.]+com/|[.]/)?[[:alnum:]]+(/?[[:alnum:]]+)*([/]|[.]html))$", 0);
+    if (status == 0){
+        printf("Regex compile success\n");
+    } else {
+        printf("Regex compile no good\n");
+    }
+
+    printf("Enter URL to retrieve information\n");
+    scanf("%s", &inputURL);
+    printf("%s\n", inputURL);
+
+    status = regexec(&rx, &inputURL, 0, NULL, 0); //does the comparison to check for valid URL that ends with html
+    if (status == 0){
+        printf("Regex matches URL\n");
+    } else {
+        printf("Regex no match with URL\n");
+    }
 }
 
 int main(){
